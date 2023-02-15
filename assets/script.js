8ec05087f6d15cd86511bab1087159804774ea3b
Sandy Bui <stbui_emory@yahoo.com>
2023-02-14 20:03:00 -0500

    fixed special characters issue

--- assets/script.js
+++ assets/script.js
@@ -12,8 +12,9 @@ var pwrequirements = {
   pwuppercase: ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L",
     "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"],
   pwnumeric: ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"],
-  pwspecial: ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", 
-    "-", "+", "=","{", "}", "[", "]", "|", "\", "/", "<", ">""]
+  pwspecial: ["!", "#", "$", "%", "&", "(", ")", "*", "+", ",", "-", 
+    ".", "/", "=", " > ", " ? ", "@", "[", "]", " ^ ", "`", 
+    "{", "|", "}", "~"],
 
 }
 
@@ -93,14 +94,14 @@ function generatePassword() {
               }
 
               if (numeric === true && pwrequirements.pwlength < pwlength) {
-                var num = pwrequirements.pwnumeric[Math.floor(Math.random() * 10)]
+                var num = pwrequirements.pwnumeric[Math.floor(Math.random() * 26)]
                 result = result + num;
                 pwrequirements.pwlength++;
               }
 
               
               if (special === true && pwrequirements.pwlength < pwlength) {
-                var sc = pwrequirements.pwspecial[Math.floor(Math.random() * 22)]
+                var sc = pwrequirements.pwspecial[Math.floor(Math.random() * 25)]
                 result = result + sc;
                 pwrequirements.pwlength++;
               }
